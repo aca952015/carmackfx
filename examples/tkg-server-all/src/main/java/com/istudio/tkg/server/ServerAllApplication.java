c68e1a72349cf5f500ba7820de2f5a065f13c822
ACA Chen <nhibernate@gmail.com>
2017-11-20 15:43:22 +0800

    examples update

---
+++ examples/tkg-server-all/src/main/java/com/istudio/tkg/server/ServerAllApplication.java
@@ -0,0 +1,12 @@
+package com.istudio.tkg.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAllApplication.class, args);
+	}
+}
